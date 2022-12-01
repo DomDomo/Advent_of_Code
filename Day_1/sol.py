de9172db7561ffafc5a7145a6195677aea92bdf1
DomDomo <g.domis02@gmail.com>
2022-12-01 12:52:10 +0000

    Solve Day 1 Part 1

--- Day_1/sol.py
+++ Day_1/sol.py
@@ -1,8 +1,18 @@
-lines = []
-with open("input.txt") as file:
-    lines = file.readlines()
-    
-for line in lines:
-    print(line)
+def get_lines(filename):
+    with open(filename) as file:
+        return file.readlines()
 
+lines = get_lines("input.txt")
+
+max_cals = 0
+temp_cals = 0
+
+for line in lines:
+    if line == '\n':
+        max_cals = max(max_cals, temp_cals)
+        temp_cals = 0
+    else:
+        temp_cals += int(line)
+
+print(max_cals)
 
