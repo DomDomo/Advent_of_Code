bb4d3c535cf969d58f9ae89c1a663f40bccaeb3f
DomDomo <g.domis02@gmail.com>
2022-12-04 11:06:32 +0000

    Solve Day 4 Part 2

--- Day_4/part2.py
+++ Day_4/part2.py
@@ -4,18 +4,15 @@ def get_lines(filename):
 
 lines = get_lines("input.txt")
 
-def contains_within(x1, x2, y1, y2):
-    return x1 <= y1 and x2 >= y2
-
-full_contain_pairs = 0
+partial_contain_pairs = 0
 
 for line in lines:
     pair1, pair2 = line.strip().split(",")
     a1, a2 = pair1.split("-")
     b1, b2 = pair2.split("-")
     a1, a2, b1, b2 = int(a1), int(a2), int(b1), int(b2)
-    if contains_within(a1, a2, b1, b2) or contains_within(b1, b2, a1, a2):
-        full_contain_pairs += 1
+    if len(set(range(a1, a2+1)) & set(range(b1, b2+1))) != 0:
+        partial_contain_pairs += 1
     
-print(full_contain_pairs)
+print(partial_contain_pairs)
 
