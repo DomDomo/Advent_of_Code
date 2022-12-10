709bd52595cc3b09bdb55cf7c3bb4ef7ce489c17
DomDomo <g.domis02@gmail.com>
2022-12-10 04:39:35 +0000

    Solve Day 7 Part 1

---
+++ Day_7/part1.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+def get_lines(filename):
+    with open(filename) as file:
+        return file.readlines()
+
+lines = get_lines("input.txt")
+threshold = 100000
+
+system = defaultdict(int)
+directory = [] 
+
+for line in lines:
+    command = line.rstrip().split(" ")
+    if command[0] == "$":
+        if command[1] == "cd":
+            if command[2] == "..":
+                directory.pop()
+            else:
+                directory.append(command[2])
+    else:
+        if command[0] != "dir":
+            size = int(command[0])
+            for i in range(len(directory)):
+                print(directory[:i+1], size)
+                system["/".join(directory[:i+1])] += size
+
+final_size = 0
+for key in system:
+    if system[key] <= threshold:
+        final_size += system[key]
+
+print(final_size)
